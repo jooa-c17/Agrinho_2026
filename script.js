98fd310ac2e4be9777d9f0c6b569a5998dad206f
jooa-c17 <hiroshighosty@gmail.com>
2026-06-14 16:35:02 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+        document.getElementById("mensagem").innerHTML =
+                "🌿 Um agro forte é aquele que produz alimentos com responsabilidade, preservando o meio ambiente e garantindo um futuro sustentável.";
+                }
